31fae8bbe2ed8cdd88d4fe4a503c2064de2b4f65
Alex Huryn <algurin77@gmail.com>
2025-02-06 23:58:18 +0100

    lesson_9

---
+++ lesson_9/js/script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const images = document.querySelectorAll(".gallery img");
+    const savedImage = localStorage.getItem("selectedImage");
+
+    if (savedImage) {
+        document.body.style.backgroundImage = `url(${savedImage})`;
+        images.forEach(img => {
+            if (img.src === savedImage) {
+                img.classList.add("selected");
+            }
+        });
+    }
+
+    images.forEach(img => {
+        img.addEventListener("click", () => {
+            document.body.style.backgroundImage = `url(${img.src})`;
+            localStorage.setItem("selectedImage", img.src);
+            images.forEach(i => i.classList.remove("selected"));
+            img.classList.add("selected");
+        });
+    });
+});
